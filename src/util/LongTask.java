f89e46ce1ac7f7acc53fdabe9c5b1e0951b4c4a9
altostratous <altostratous@gmail.com>
2016-11-21 23:27:04 +0330

    Added ui and util interfaces.

---
+++ src/util/LongTask.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Interface for all tasks that could be manage by the runner app.
+ * To run and manage your task, you have to implement this interface
+ * and add the *.class file to the runner application.
+ */
+public interface LongTask {
+}
